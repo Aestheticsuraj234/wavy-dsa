9b9c22b3a0fd650e45bc4e8a8b0353e8f591d2be
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-03-03 14:40:38 +0530

    Update ChatBubbleWing pathClassName in services.tsx

--- app/(landing)/_components/design/services.tsx
+++ app/(landing)/_components/design/services.tsx
@@ -21,7 +21,10 @@ export const PhotoChatMessage = () => {
     <div className="absolute top-8 right-8 max-w-[17.5rem] py-6 px-8 bg-black rounded-t-xl rounded-bl-xl font-code text-base lg:top-16 lg:right-[8.75rem] lg:max-w-[17.5rem]">
       Hey Brainwave, enhance this photo
       {/* @ts-ingore */}
-      <ChatBubbleWing  className="absolute left-full bottom-0" />
+      <ChatBubbleWing  pathClassName={
+        "fill-black"
+      
+      } className="absolute left-full bottom-0" />
     </div>
   );
 };
